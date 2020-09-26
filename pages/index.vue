1ea66f6c76f1b5b2055ab4dd40e2e5bf0c4db9a0
Tom Richter <tomrichter99@gmail.com>
2020-09-26 17:04:38 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class PageIndex extends Vue {
+  message: string = 'This is a message'
+}
+</script>
