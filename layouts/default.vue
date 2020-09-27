e0d940f28b632f68d91117e8cc4d691ca9a496f6
Tom Richter <tomrichter99@gmail.com>
2020-09-27 07:26:18 +0200

    add navbar, footer, contact

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,42 @@
 <template>
   <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>
+        <v-btn text rounded to="/">Home</v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text rounded v-bind="attrs" v-on="on">Geocaching</v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-btn text to="/">GPX-Builder</v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn text to="/">Mehrfach-Peilung</v-btn>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text rounded v-bind="attrs" v-on="on">Diverses</v-btn>
+        </template>
+        <v-list>
+          <v-btn text to="/">Major System Trainer</v-btn>
+        </v-list>
+      </v-menu>
+      <v-btn text rounded to="/contact">Kontakt</v-btn>
+    </v-app-bar>
+
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
+
+    <v-footer padless class="font-weight-medium">
+      <v-col class="text-center" cols="12"> Impressum | Datenschutz </v-col>
+    </v-footer>
   </v-app>
 </template>
---
+++ pages/contact/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Kontakt</h1>
+    <p>
+      Du hast Fragen, Ideen oder Verbesserungsvorschläge? Schreib mir einfach eine E-Mail an:
+      <strong>tomrichter.dev@gmail.com</strong>
+    </p>
+  </div>
+</template>
