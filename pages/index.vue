70588ca2e0f72cef6632b97352b1c7a9ce3e4835
Tom Richter <tomrichter99@gmail.com>
2020-09-28 22:09:37 +0200

    add home page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Willkommen auf meiner Website!</h1>
+    <p>
+      Du findest hier einige nützliche Tools, wie etwa den GPX-Builder (für Geocacher) oder Major
+      System Trainer. Für die Zukunft sind weitere Tools und ein Fahrrad-Reiseblog geplant. Auf
+      meinem YouTube-Kanal
+      <a href="https://www.youtube.com/channel/UC0A0FkyRVnGIDtNf1kTnF6g" target="_blank">
+        BlakeX
+      </a>
+      findet ihr Videos von meiner Radtour durch Südostasien. Das Bild wurde in Saysomboun, Laos
+      aufgenommen.
+    </p>
+    <img
+      src="https://s3.eu-central-1.amazonaws.com/tomrichter.dev/laos.jpg"
+      alt="Radtour in Laos"
+      width="100%"
+    />
   </div>
 </template>
 
@@ -9,7 +24,7 @@ import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component({
   head: {
-    title: 'Home - Tom Richter',
+    title: 'Tom Richter',
   },
 })
 export default class Home extends Vue {}
